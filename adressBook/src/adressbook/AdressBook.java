a856d2a60a61c957471d63bdf94bfdcabd92ef98
Motalib Hossain <motalibhossainrbk33@gmail.com>
2019-12-15 02:12:34 +0600

    java project
    Address Book

---
+++ adressBook/src/adressbook/AdressBook.java
@@ -0,0 +1,14 @@
+
+package adressbook;
+
+
+public class AdressBook {
+
+  
+    public static void main(String[] args) {
+      login l= new login();
+      l.setVisible(true);
+      
+    }
+    
+}
